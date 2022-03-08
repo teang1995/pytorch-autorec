bd1aa38469027bf1fc2491334a4b9695eabbfcd3
teang1995 <teang1995@ajou.ac.kr>
2022-03-08 08:17:41 +0100

    [#3]load table from file at datasets/dataset.py

--- datasets/dataset.py
+++ datasets/dataset.py
@@ -1,21 +1,57 @@
 import pandas as pd
 from typing import Optional
 from torch.utils.data import Dataset
+import os
 
 
 class MovieLensDataset(Dataset):
+
     def __init__(self,
                 data_dir: str,
-                device: str,
-                data_type: str):
-        # TODO : data_type validation -> user, item 중 하나로.
-        raise NotImplementedError
+                device: str='cuda:0',
+                model_type: str):
+
+        self.data_type = data_type
+        self.model_type = model_type
+        self.data = None
+
+        movie_path = os.path.join(data_dir, 'movies.dat')
+        rating_path = os.path.join(data_dir, 'ratings.dat')
+        user_path = ps.path.join(data_dir, 'users.dat')
+
+        movie_cols = ['movie_id', 'title', 'genres']
+        movies = pd.read_table(movie_path, 
+                               sep='::',
+                               header=None,
+                               names=movie_cols,
+                               engine='python',
+                               encoding='ISO-8859-1')
+
+        rating_cols = ['user_id', 'movie_id', 'rating', 'timestamp']
+        ratings = pd.read_table(rating_path,
+                              sep='::',
+                              names=rating_cols,
+                              engine='python',
+                              encoding='ISO-8859-1')
+
+        user_cols = ['user_id', 'gender', 'age', 'occupation', 'zip']
+        users = pd.read_table(user_path,
+                              sep='::',
+                              names=rating_cols,
+                              engine='python',
+                              encoding='ISO-8859-1')
+
+        if  self.model_type = 'user':
+            self.data = self.build_items
+        else:
+            self.data = self.build_users
 
     @staticmethod
     def build_items(items: pd.DataFrame,
                     ratings: pd.DataFrame,
                     users: pd.DataFrame):
         # TODO : i-autorec dataset build 깔끔 구현
+        # TODO : movielens-1M, movielens-10M 데이터 불러오는 게 다르다면 어떻게 할 지 결정해야 함.
         raise NotImplementedError
         
     @staticmethod
@@ -26,12 +62,11 @@ class MovieLensDataset(Dataset):
         raise NotImplementedError
 
     def __getitem__(self, idx):
-        # TODO : getitem에서 함수 받기? 아니면 여기선 return self.data[idx] 만 해주기? 결정해야 함
-        raise NotImplementedError
+        return self.data['idx']
 
     def __len__(self):
         # return len(self.data)
-        raise NotImplementedError
+        return len(self.data)
     
 
 def main():
