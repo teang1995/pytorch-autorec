bab8350772c072bfb11b91712146001ba07147f5
teang1995 <teang1995@ajou.ac.kr>
2022-03-10 06:23:59 +0100

    [#3] write test code for movielensdataset

--- datasets/dataset.py
+++ datasets/dataset.py
@@ -3,75 +3,52 @@ from typing import Optional
 from torch.utils.data import Dataset
 import os
 
+from autorec.path import MOVIELENS_1M_DIR, MOVIELENS_10M_DIR
+
 
 class MovieLensDataset(Dataset):
 
     def __init__(self,
                 data_dir: str,
                 device: str='cuda:0',
-                model_type: str):
+                model_type: str='item'):
 
-        self.data_type = data_type
+        assert model_type in ['user', 'item'], "model type must be user of item"
+
+        self.device = device
         self.model_type = model_type
         self.data = None
 
-        movie_path = os.path.join(data_dir, 'movies.dat')
+        # set data dirs
         rating_path = os.path.join(data_dir, 'ratings.dat')
-        user_path = ps.path.join(data_dir, 'users.dat')
 
-        movie_cols = ['movie_id', 'title', 'genres']
-        movies = pd.read_table(movie_path, 
-                               sep='::',
-                               header=None,
-                               names=movie_cols,
-                               engine='python',
-                               encoding='ISO-8859-1')
-
+        # read rating file
         rating_cols = ['user_id', 'movie_id', 'rating', 'timestamp']
-        ratings = pd.read_table(rating_path,
+        self.ratings = pd.read_table(rating_path,
                               sep='::',
                               names=rating_cols,
                               engine='python',
                               encoding='ISO-8859-1')
 
-        user_cols = ['user_id', 'gender', 'age', 'occupation', 'zip']
-        users = pd.read_table(user_path,
-                              sep='::',
-                              names=rating_cols,
-                              engine='python',
-                              encoding='ISO-8859-1')
-
-        if  self.model_type = 'user':
-            self.data = self.build_items
+        if  self.model_type == 'item':
+            self.data = pd.pivot_table(self.ratings, index=['movie_id'], values=['rating'], columns='user_id', fill_value=0)
         else:
-            self.data = self.build_users
-
-    @staticmethod
-    def build_items(items: pd.DataFrame,
-                    ratings: pd.DataFrame,
-                    users: pd.DataFrame):
-        # TODO : i-autorec dataset build 깔끔 구현
-        # TODO : movielens-1M, movielens-10M 데이터 불러오는 게 다르다면 어떻게 할 지 결정해야 함.
-        raise NotImplementedError
-        
-    @staticmethod
-    def build_users(items: pd.DataFrame,
-                    ratings: pd.DataFrame,
-                    users: pd.DataFrame):
-        # TODO : u-autorec dataset build 깔끔 구현
-        raise NotImplementedError
+            self.data = pd.pivot_table(self.ratings, index=['user_id'], values=['rating'], columns='movie_id', fill_value=0)
 
     def __getitem__(self, idx):
         return self.data['idx']
 
     def __len__(self):
-        # return len(self.data)
         return len(self.data)
     
 
 def main():
-    # TODO: write test code
-    raise NotImplementedError
+    data_dir = MOVIELENS_1M_DIR
+    movielens_dataset = MovieLensDataset(data_dir=data_dir,
+                                         device='cuda:0',
+                                         model_type='item')
+    print(type(movielens_dataset.data))
+    print(len(movielens_dataset.data))
 
 if __name__ == "__main__":
     main()
