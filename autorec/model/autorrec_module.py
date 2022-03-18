f345459f7827e21675aefb89076f27a2e0b79a26
teang1995 <teang1995@ajou.ac.kr>
2022-03-18 16:21:04 +0000

    [#15] implement autorec_module.py

--- autorec/model/autorrec_module.py
+++ autorec/model/autorrec_module.py
@@ -1,24 +1,35 @@
 import pytorch_lightning as pl
-
+from autorec.model.loss import masked_RMSE
 
 class AutoRecModule(pl.LightningDataModule):
     def __init__(self,
                 init_lr: float,
                 input_size: int,
                 hidden_size: int)
-        raise NotImplementedError
+        self.init_lr = init_lr
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+
+        self.net = AutoRec(input_size=self.input_size,
+                           hidden_size=self.hidden_size)
 
     def forward(self, X):
-        raise NotImplementedError
+        retrun self.net(X)
 
-    def training_step(self, batch):
-        raise NotImplementedError
+    def training_step(self, X):
+        X = X.float()
+        prediction = self.forward(X)
+        loss = self.masked_RMSE(prediction, X)
+        self.log('train loss', loss)
 
     def validation_step(self, batch):
-        raise NotImplementedError
+        X = X.float()
+        prediction = self.forward(X)
+        loss = self.masked_RMSE(prediction, X)  
+        self.log('valid loss', loss)
 
     def test_step(self, batch):
         raise NotImplementedError
 
     def configure_optimizers(self):
-        raise NotImplementedError
+        raise torch.optim.Rprop(self.net.parameters(), lr=self.init_lr)
--- autorec/train.py
+++ autorec/train.py
@@ -1,10 +1,15 @@
 import hydra
 from omegaconf import DictConfig
 import pytorch_lightning as pl
+from pytorch_lightning.loggers import TensorBoardLogger
+from pytorch_lightning.callbacks.early_stopping import EarlyStopping
+from pytorch_lightning.callbacks import ModelCheckpoint
+
+from autorec.model.autorec_module import AutoRecModule
+from autorec.datasets.datamodule import MovieLensDataModule
+
 
 HYDRA_FULL_ERROR=1 
-
-# TODO: get hyperparameter using hydra
 @hydra.main(config_path="config", config_name="train_config")
 def main(cfg: DictConfig):
     hp_config = cfg['train']
@@ -17,17 +22,31 @@ def main(cfg: DictConfig):
     train_path = data_config['train_path']
     valid_path = data_config['valid_path']
 
-    print(epoch)
-    print(batch_size)
-    print(optimizer)
-    print(train_path)
-    print(valid_path)
+    
+    logger = TensorBoardLogger(
+                            save_dir="autorec",  
+                            name="movielens-autorec", 
+                            default_hp_metric=False,
+                            )
 
+    checkpoint_callback = ModelCheckpoint(
+                                        monitor='Accuracy',  # 어떤 지표를 기준으로 삼을 것인지
+                                        dirpath='classification_ckpt',  # 체크포인트 파일들이 저장될 경로
+                                        filename='cats&dogs_clssification',  # 체크포인트 파일의 이름(확장자 불요)
+                                        mode='max'  # 위 지표가 최대일 때의 모델을 저장(min으로 설정 가능)
+                                        )
+
+
+    early_stop_callback = EarlyStopping(
+                                        monitor='Accuracy',  # 어떤 지표를 기준으로 삼을 것인지
+                                        min_delta=1e-4,  # 위 지표가 얼마나 향상이 되어야 하는지
+                                        patience=20,  # 몇 에포크동안 지켜볼 것인지
+                                        mode='max'
+                                        )
     # TODO: add data_type, model_type to config
     data_type = '1M'
     model_type = 'item'
-    assert data_type in ['1M', '10M'], "data type must be 1M or 10M"
-    assert model_type in ['user', 'item'], "model_type must be user or item"
+    
 
 if __name__ == "__main__":
     main()
