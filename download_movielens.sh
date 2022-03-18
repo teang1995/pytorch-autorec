d43b0552e45a46c6b53cefb53a0020a7d2d79cde
teang1995 <teang1995@ajou.ac.kr>
2022-03-18 12:15:25 +0000

    [#3] edit dockerfile and download_movielens.sh

--- download_movielens.sh
+++ download_movielens.sh
@@ -6,8 +6,8 @@ wget https://files.grouplens.org/datasets/movielens/ml-1m.zip
 wget https://files.grouplens.org/datasets/movielens/ml-10m.zip
 
 # unzip file
-unzip ml-1m.zip -d autorec/data/
-unzip ml-10m.zip -d autorec/data/
+unzip ml-1m.zip -d /
+unzip ml-10m.zip -d /
 
 # delete zip file
 rm ml-1m.zip
