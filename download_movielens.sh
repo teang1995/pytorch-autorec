9812df6dd7f44dd33f8db2975328e79f415fae27
teang1995 <teang1995@ajou.ac.kr>
2022-03-18 11:02:40 +0000

    [#3]edit download_movielens.sh

--- download_movielens.sh
+++ download_movielens.sh
@@ -6,8 +6,8 @@ wget https://files.grouplens.org/datasets/movielens/ml-1m.zip
 wget https://files.grouplens.org/datasets/movielens/ml-10m.zip
 
 # unzip file
-unzip ./autorec/data/ml-1m.zip -d autorec/data/
-unzip ./autorec/data/ml-10m.zip -d autorec/data/
+unzip ml-1m.zip -d autorec/data/
+unzip ml-10m.zip -d autorec/data/
 
 # delete zip file
 rm ml-1m.zip
